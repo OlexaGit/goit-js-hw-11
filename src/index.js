f914ed406f3d1c7bca719438c2248a37db9fcdaa
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-23 17:09:05 +0300

    axios

--- src/index.js
+++ src/index.js
@@ -3,86 +3,117 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchCountries } from './fetchCountries';
+import axios from 'axios';
 
-const DEBOUNCE_DELAY = 300;
+const form = document.querySelector('#search-form');
 
-const fieldFindCountry = document.querySelector('#search-box');
-const listСountry = document.querySelector('.country-list');
-const infoCountry = document.querySelector('.country-info');
+form.addEventListener('submit', handleSubmit);
 
-listСountry.style.fontSize = '24px';
-
-fieldFindCountry.addEventListener(
-  'input',
-  debounce(
-    event => {
-      const elementFind = event.target.value.trim();
-      if (elementFind === '') {
-        clear();
-        return;
-      }
-      fetchCountries(elementFind)
-        .then(countries => {
-          if (countries.length > 10) {
-            clear();
-            Notiflix.Notify.info(
-              'Too many matches found. Please enter a more specific name.'
-            );
-            return;
-          } else {
-            if (countries.length === 1) {
-              listСountry.innerHTML = '';
-              renderInfoOneCountry(countries);
-            } else {
-              infoCountry.innerHTML = '';
-              renderCountrybetweenTwoAndTen(countries);
-            }
-          }
-        })
-        .catch(error => {
-          clear();
-          Notiflix.Notify.failure('Oops, there is no country with that name');
-        });
-    },
-    DEBOUNCE_DELAY
-    // {
-    //   leading: true,
-    //   trailing: true,
-    // }
-  )
-);
-
-function renderCountrybetweenTwoAndTen(countries) {
-  // console.log(countries);
-  const markup = countries
-    .flatMap(({ flags: { png, alt }, name }) => {
-      return `
-      <li><img src="${png}" width="30" alt="${alt}"></img>  ${name.official}</li>`;
-    })
-    .join('');
-  listСountry.innerHTML = markup;
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { searchQuery },
+  } = event.currentTarget;
+  if (searchQuery.value === '') {
+    event.currentTarget.reset();
+    return;
+  }
+  getsearchQuery(searchQuery.value);
+  console.log(searchQuery.value);
 }
 
-function renderInfoOneCountry(countries) {
-  // console.log(countries[0].currencies);
-  const ArrayOjectCurrencies = Object.values(countries[0].currencies);
-  const currenciesName = ArrayOjectCurrencies.map(elem => elem.name).join(', ');
-  const languagesName = Object.values(countries[0].languages).join(', ');
-  // console.log(currenciesName);
-  const markup = countries
-    .flatMap(({ flags: { png, alt }, name, capital, population }) => {
-      return `        
-      <div style="font-size:30px"><img src="${png}" width="30" alt="${alt}"></img>  ${name.official}</div>
-      <div style="font-weight:400"><span style="font-weight:700">Capital:</span> ${capital}</div>
-      <div style="font-weight:400"><span style="font-weight:700">Currencies:</span> ${currenciesName}</div>
-      <div style="font-weight:400"><span style="font-weight:700">Population:</span> ${population}</div>
-      <div style="font-weight:400"><span style="font-weight:700">Languages:</span> ${languagesName}</div>`;
-    })
-    .join('');
-  infoCountry.innerHTML = markup;
+// const myApiKey = 'u_lbe459kmo7';
+// const axios = require('axios');
+// axios.get('/users').then(res => {
+//   console.log(res.data);
+// });
+async function getsearchQuery(searchEl) {
+  console.log(searchEl);
+  // let totalHits = 0;
+  // const myApiKey = 'u_lbe459kmo7';
+  // const imageType = 'photo';
+  // const orientation = 'horizontal';
+  // const safesearch = 'true';
+  try {
+    const myApiKey = 'u_lbe459kmo7';
+    const imageType = 'photo';
+    const orientation = 'horizontal';
+    const safesearch = 'true';
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=35687240-9029e9ca17f641307dafe05a9&q=${searchEl}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}`
+    );
+    // pixabay.com/api/?key=35687240-9029e9ca17f641307dafe05a9&q=yellow+flowers&image_type=photo
+    console.log(response.data);
+    console.log('Загальна кількість зображень: ', response.data.totalHits);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function clear() {
-  listСountry.innerHTML = '';
-  infoCountry.innerHTML = '';
-}
+// fieldFindCountry.addEventListener(
+//   'input',
+//   debounce(event => {
+//     const elementFind = event.target.value.trim();
+//     if (elementFind === '') {
+//       clear();
+//       return;
+//     }
+//     fetchCountries(elementFind)
+//       .then(countries => {
+//         if (countries.length > 10) {
+//           clear();
+//           Notiflix.Notify.info(
+//             'Too many matches found. Please enter a more specific name.'
+//           );
+//           return;
+//         } else {
+//           if (countries.length === 1) {
+//             listСountry.innerHTML = '';
+//             renderInfoOneCountry(countries);
+//           } else {
+//             infoCountry.innerHTML = '';
+//             renderCountrybetweenTwoAndTen(countries);
+//           }
+//         }
+//       })
+//       .catch(error => {
+//         clear();
+//         Notiflix.Notify.failure('Oops, there is no country with that name');
+//       });
+//   }, 300)
+// );
+
+// function renderCountrybetweenTwoAndTen(countries) {
+//   // console.log(countries);
+//   const markup = countries
+//     .flatMap(({ flags: { png, alt }, name }) => {
+//       return `
+//       <li><img src="${png}" width="30" alt="${alt}"></img>  ${name.official}</li>`;
+//     })
+//     .join('');
+//   listСountry.innerHTML = markup;
+// }
+
+// function renderInfoOneCountry(countries) {
+//   // console.log(countries[0].currencies);
+//   const ArrayOjectCurrencies = Object.values(countries[0].currencies);
+//   const currenciesName = ArrayOjectCurrencies.map(elem => elem.name).join(', ');
+//   const languagesName = Object.values(countries[0].languages).join(', ');
+//   // console.log(currenciesName);
+//   const markup = countries
+//     .flatMap(({ flags: { png, alt }, name, capital, population }) => {
+//       return `
+//       <div style="font-size:30px"><img src="${png}" width="30" alt="${alt}"></img>  ${name.official}</div>
+//       <div style="font-weight:400"><span style="font-weight:700">Capital:</span> ${capital}</div>
+//       <div style="font-weight:400"><span style="font-weight:700">Currencies:</span> ${currenciesName}</div>
+//       <div style="font-weight:400"><span style="font-weight:700">Population:</span> ${population}</div>
+//       <div style="font-weight:400"><span style="font-weight:700">Languages:</span> ${languagesName}</div>`;
+//     })
+//     .join('');
+//   infoCountry.innerHTML = markup;
+// }
+
+// function clear() {
+//   listСountry.innerHTML = '';
+//   infoCountry.innerHTML = '';
+// }
