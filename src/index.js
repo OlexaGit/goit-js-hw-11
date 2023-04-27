e7e5fa1fe1e92127a10a101150768039114a47d4
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-27 22:51:14 +0300

    button is hidden

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-// import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import NewsApiService from './on-search';
-// import axios from 'axios';
 
 const form = document.querySelector('#search-form');
 const galleryContainer = document.querySelector('.gallery');
@@ -13,7 +11,6 @@ loadMoreBtn.style.display = 'none';
 
 form.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', onLoadMore);
-console.log(newsApiService.isBtnVisible);
 
 function onSearch(e) {
   e.preventDefault();
@@ -26,7 +23,13 @@ function onSearch(e) {
   }
   newsApiService.resetPage();
   newsApiService.fetchArticles().then(hits => {
+    if (newsApiService.isBtnVisible) {
+      return;
+    }
     renderGallery(hits);
+    if (newsApiService.isBtnVisible) {
+      loadMoreBtn.style.display = 'none';
+    }
   });
 }
 
@@ -90,116 +93,5 @@ function renderGallery(hits) {
 
 function clearContainer() {
   galleryContainer.innerHTML = '';
+  loadMoreBtn.style.display = 'none';
 }
-
-// import Notiflix from 'notiflix';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import { fetchCountries } from './fetchCountries';
-// import axios from 'axios';
-
-// const form = document.querySelector('#search-form');
-// const galleryContainer = document.querySelector('.gallery');
-// const loadMoreBtn = document.querySelector('.load-more');
-
-// let searchEl = '';
-// let page = 1;
-// loadMoreBtn.style.display = 'none';
-// form.addEventListener('submit', handleSubmit);
-// loadMoreBtn.addEventListener('click', () => {
-//   page += 1;
-//   getsearchQuery(searchEl, page);
-// });
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const {
-//     elements: { searchQuery },
-//   } = event.currentTarget;
-//   if (searchQuery.value === '') {
-//     event.currentTarget.reset();
-//     return;
-//   }
-
-//   galleryContainer.innerHTML = '';
-//   searchEl = searchQuery.value;
-//   getsearchQuery(searchEl, page);
-// }
-
-// async function getsearchQuery(searchEl, page = 1) {
-//   try {
-//     const per_page = 6;
-//     const myApiKey = '35687240-9029e9ca17f641307dafe05a9';
-//     const imageType = 'photo';
-//     const orientation = 'horizontal';
-//     const safesearch = 'true';
-//     const response = await axios.get(
-//       `https://pixabay.com/api/?key=${myApiKey}&q=${searchEl}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}&page=${page}&per_page=${per_page}`
-//     );
-//     console.log(response);
-//     console.log('Загальна кількість зображень: ', response.data.totalHits);
-//     const lengthArray = response.data.hits.length;
-//     if (lengthArray === 0) {
-//       Notiflix.Notify.failure(
-//         'Sorry, there are no images matching your search query. Please try again.'
-//       );
-//       // event.reset();
-//       return;
-//     }
-//     Notiflix.Notify.info(`Hooray! We found ${response.data.totalHits} images.`);
-//     renderGallery(response.data.hits);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// function renderGallery(array) {
-//   const markup = array
-//     .map(
-//       ({
-//         webformatURL,
-//         largeImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => {
-//         return `
-//     <div class="photo-card">
-//         <a class="gallery__link" href="${largeImageURL}">
-//           <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//         </a>
-//       <div class="info">
-//         <p class="info-item">
-//           <b>Likes</b>
-//           ${likes}
-//         </p>
-//         <p class="info-item">
-//           <b>Views</b>
-//           ${views}
-//         </p>
-//         <p class="info-item">
-//           <b>Comments</b>
-//           ${comments}
-//         </p>
-//         <p class="info-item">
-//           <b>Downloads</b>
-//           ${downloads}
-//         </p>
-//       </div>
-//     </div>
-//   `;
-//       }
-//     )
-//     .join('');
-
-//   loadMoreBtn.style.display = 'inline-block';
-//   galleryContainer.insertAdjacentHTML('beforeend', markup);
-
-//   /* SimpleLightbox */
-//   const lightbox = new SimpleLightbox('.gallery a', {
-//     captionDelay: 100,
-//   });
-//   lightbox.refresh();
-// }
--- src/on-search.js
+++ src/on-search.js
@@ -1,11 +1,10 @@
-const per_page = 40;
+const per_page = 20;
 const myApiKey = '35687240-9029e9ca17f641307dafe05a9';
 const imageType = 'photo';
 const orientation = 'horizontal';
 const safesearch = 'true';
 const axios = require('axios').default;
 
-// import axios from 'axios';
 import Notiflix from 'notiflix';
 export default class NewsApiService {
   constructor() {
@@ -15,31 +14,34 @@ export default class NewsApiService {
     this.isBtnVisible = false;
   }
 
-  fetchArticles() {
-    return axios
-      .get(
-        `https://pixabay.com/api/?key=${myApiKey}&q=${this.searchQuery}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}&page=${this.page}&per_page=${per_page}`
-      )
-      .then(({ data: { hits }, data: { totalHits } }) => {
-        const lengthArray = totalHits;
-        this.totalPages = totalHits / per_page;
-        if (lengthArray === 0) {
-          this.resetPage();
-          Notiflix.Notify.failure(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-          return;
-        } else if (this.page === 1) {
-          Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
-        }
-        console.log(this.page, this.totalPages);
-        if (this.page > this.totalPages) {
-          this.isBtnVisible = true;
-          this.toggleAlertPopup();
-        }
-        this.incrementPage();
-        return hits;
-      });
+  async fetchArticles() {
+    try {
+      return await axios
+        .get(
+          `https://pixabay.com/api/?key=${myApiKey}&q=${this.searchQuery}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}&page=${this.page}&per_page=${per_page}`
+        )
+        .then(({ data: { hits }, data: { totalHits } }) => {
+          const lengthArray = totalHits;
+          this.totalPages = totalHits / per_page;
+          if (lengthArray === 0) {
+            this.isBtnVisible = true;
+            Notiflix.Notify.failure(
+              'Sorry, there are no images matching your search query. Please try again.'
+            );
+            return;
+          } else if (this.page === 1) {
+            Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+          }
+          if (this.page > this.totalPages) {
+            this.isBtnVisible = true;
+            this.toggleAlertPopup();
+          }
+          this.incrementPage();
+          return hits;
+        });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   incrementPage() {
@@ -56,12 +58,13 @@ export default class NewsApiService {
       Notiflix.Notify.failure(
         "We're sorry, but you've reached the end of search results."
       );
-    }, 3000);
+    }, 1000);
   }
 
   get query() {
     return this.searchQuery;
   }
+
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
